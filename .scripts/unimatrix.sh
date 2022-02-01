7aab4bac7e4b3fcef1d195a3cca7268ac59774e8
vtog <vince@tognaci.net>
2022-02-01 11:12:48 -0600

    playing with custom unimatrix settings

--- .scripts/unimatrix.sh
+++ .scripts/unimatrix.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-alacritty -o window.startup_mode=fullscreen -o window.opacity=1 -o font.size=20 -e unimatrix
+alacritty -o window.startup_mode=fullscreen -o window.opacity=1 -o font.size=16 -e unimatrix -s 87 -l knnss
