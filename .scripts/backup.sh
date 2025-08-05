cf9bccf00550e6f1fbbe94cfb41f2df11e63ff6e
vtog <vince@tognaci.net>
2025-08-05 08:37:10 -0500

    changed backup script name

---
+++ .scripts/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+backupFile=$HOME/$(date +'%Y%m%d-'$(hostname --short)'-backup.tar.gz')
+
+list=$(ls -d $HOME/libvirt/ -d $HOME/OCP/ -d /etc/named/ -f /mirror/oc-mirror/*.{sh,yaml} \
+  -f /etc/named.conf -f /etc/chrony.conf -f /etc/hosts -f /opt/AdGuardHome/AdGuardHome.yaml \
+  2>/dev/null)
+echo "\nFound the following for backup:\n\n$list\n"
+
+echo $list | xargs tar --exclude="*.tar.gz" --exclude "*tar" --exclude="*.iso" --exclude="*.raw" \
+  --exclude="*.qcow2" --exclude="*.img" --exclude="openshift-install*" --exclude="cache" \
+  --exclude="temp" -czvvf $backupFile \
+  2>backup_errors.txt
+
+#tar --exclude="*.tar.gz" --exclude "*tar" --exclude="*.iso" -czvf $backupFile \
+#$HOME/libvirt $HOME/OCP /etc/named /etc/named.conf /etc/chrony.conf /etc/hosts /opt/AdGuardHome/AdGuardHome.yaml
+
+if [ -d /mnt/perl ]; then
+  mv $backupFile /mnt/perl
+fi
