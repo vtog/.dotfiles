91966883933fac0fdb37ef38524ddd7194519c8c
vtog <vince@tognaci.net>
2025-08-05 09:06:31 -0500

    more cleanup

--- .scripts/backup.sh
+++ .scripts/backup.sh
@@ -2,15 +2,16 @@
 
 backupFile=$HOME/$(date +'%Y%m%d-'$(hostname --short)'-backup.tar.gz')
 
-list=$(ls -d $HOME/libvirt/ -d $HOME/OCP/ -d /etc/named/ -f /etc/named.conf \
-  -f /etc/chrony.conf -f /etc/hosts \
+list=$(ls -d $HOME/libvirt/ -d $HOME/OCP/ \
+  -d /etc/named/ -f /etc/named.conf -f /etc/chrony.conf -f /etc/hosts \
   -f /mirror/oc-mirror/*.{sh,yaml} -f /opt/AdGuardHome/AdGuardHome.yaml \
   2>/dev/null)
 echo -e "\nFound the following for backup:\n\n$list\n"
 
-echo $list | xargs tar --exclude="*.tar.gz" --exclude "*tar" --exclude="*.iso" --exclude="*.raw" \
-  --exclude="*.qcow2" --exclude="*.img" --exclude="openshift-install*" --exclude="cache" \
-  --exclude="temp" -czvvf $backupFile \
+echo $list | xargs tar --exclude="*.tar.gz" --exclude "*tar" --exclude="*.iso" \
+  --exclude="*.raw" --exclude="*.qcow2" --exclude="*.img" \
+  --exclude="openshift-install*" --exclude="cache" --exclude="temp" \
+  -czvvf $backupFile \
   2>backup_errors.txt
 
 #tar --exclude="*.tar.gz" --exclude "*tar" --exclude="*.iso" -czvf $backupFile \
